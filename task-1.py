eeabb77bbe6f7104b32a323fd44da020c0ba5135
Xaivntaaj Her <dn_her@yahoo.com>
2023-03-28 13:22:19 -0500

    updated def function

--- task-1.py
+++ task-1.py
@@ -1,9 +1,13 @@
 # Reverse a String
+
 word = input('Enter a word: ')
 
-reversed_word = ''
+def words():
+    reversed_word = ''
 
-for index in range(len(word)-1,-1,-1):
-    reversed_word += word[index]
+    for index in range(len(word)-1,-1,-1):
+        reversed_word += word[index]
 
-print(reversed_word)
+    print(reversed_word)
+
+words()
