5692e5f9c41346ae740bba865b46f94b4dc6e388
Xaivntaaj Her <dn_her@yahoo.com>
2023-03-28 13:48:59 -0500

    Capitalize letter function added

---
+++ task-2.py
@@ -0,0 +1,16 @@
+# Capitalize a Letter
+
+word = input('Enter a word: ')
+
+def words():
+    capitalize_word = ''
+
+    capitalize_word = word.title()
+
+    print(capitalize_word)
+
+words()
+
+txt = 'hello world, I love to eat food'
+x = txt.title()
+print(x)
