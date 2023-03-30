a3b67a09e8cea4cdab615d09dbb5106aefe1e977
Xaivntaaj Her <dn_her@yahoo.com>
2023-03-30 09:02:45 -0500

    created contionals for prime numbers

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+# A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. 
+# The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+# Write a method that does the Fibonacci sequence starting at 1
+
+# 1 + 1 = 2
+# 1 + 2 = 3
+# 2 + 3 = 5
+# 3 + 5 = 8
+
+
+num1 = 0
+num2 = 1
+
+for num in range(1,9):
+    
+    sum = num1 + num2
+
+    results = ()
+
+    print(results)
+   
+
+
+
--- prime numbers.py
+++ prime numbers.py
@@ -2,7 +2,27 @@
 # Write a method that prints out all prime numbers between 1 and 100 
 # for loop range
 # conditionals
+# print
 
 
-for number in range(1,101):
-   
+for num in range(1,101):
+    is_prime = True
+    for num2 in range(2,num):
+        remainder = num % num2
+        if remainder == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
+
+
+
+
+# modulus
+# x = 43
+# y = 1
+# result = x % y
+
+# print(result)
+
+
